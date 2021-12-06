ad3e418173a051a1903072ad603207e94a089e7b
mobada <46624769+mobada@users.noreply.github.com>
2021-12-06 08:21:16 +0300

    KSA Zakat QR phase 1 base64 converter
    This is an DB function to get 5 tags required in phase 1 from KSA Zakat and convert them to accepted QR format.

---
+++ get_inv_qr_base64.sql
@@ -0,0 +1,64 @@
+create or replace function get_inv_qr_base64(p_tag1 varchar2,p_tag2 varchar2,p_tag3 varchar2,p_tag4 varchar2,p_tag5 varchar2) return varchar2
+/*Function to return  base64 to be used in qr code for electronic invoice as Zatca.gov.sa phase 1
+Mobada 05-12-2021
+tag1 : Seller name, it could be in english or arabic, so I user lengthb to count its length by bytes because arabic charaters strored in 2 bytes not one as english
+tag2 : Tax registeration number
+tag3 : Invoice date & time stamp
+tag4 : Total invoice amount including VAT, preferred to be 2 decimal digits as they do in thier example
+tag5 : Tax amount, preferred to be 2 decimal digits as they do in thier example
+
+tips:
+1. Use lengthb for arabic cahcartes.
+2. Use to_char(number,'0x') to convert numbers of tag and tage vaule length also into hex before convert all text to base64, if you use normal conversion it will be another number it will deal
+with 1 number as example as a character not 1 as a number it will be 31 not 01
+3. We should convert all values to hex before concatenated them then convert the all text to base64
+4. Then we should extract base64 as varchar2 to be in the right frmat wanted by Zatca.gov.sa
+*/
+is
+l_tag1 varchar2(4000) :=p_tag1;
+l_tag1_len number;
+
+l_tag2 varchar2(4000) :=p_tag2;
+l_tag2_len number;
+
+l_tag3 varchar2(4000) :=p_tag3;
+l_tag3_len number;
+
+l_tag4 varchar2(4000) :=p_tag4;
+l_tag4_len number;
+
+l_tag5 varchar2(4000) :=p_tag5;
+l_tag5_len number;
+
+l_qr_hex varchar2(32000);
+l_qr_hex_raw  raw(500);
+l_qr_base64 varchar2(500);
+
+begin
+
+--Get tags length bu using lengthb because of arabic characters is stored in two bytes not one as english characters
+l_tag1_len := lengthb(l_tag1);
+l_tag2_len := lengthb(l_tag2);
+l_tag3_len := lengthb(l_tag3);
+l_tag4_len := lengthb(l_tag4);
+l_tag5_len := lengthb(l_tag5);
+
+--Convert tags to hex before concatenate
+l_tag1 := rawtohex(utl_raw.cast_to_raw(l_tag1));
+l_tag2 := rawtohex(utl_raw.cast_to_raw(l_tag2));
+l_tag3 := rawtohex(utl_raw.cast_to_raw(l_tag3));
+l_tag4 := rawtohex(utl_raw.cast_to_raw(l_tag4));
+l_tag5 := rawtohex(utl_raw.cast_to_raw(l_tag5));
+
+--Concatenate the the hex string, I will usre to_char to convert number of tag and its length to hex
+l_qr_hex := to_char(1,'0x')||to_char(l_tag1_len,'0x')||l_tag1||
+            to_char(2,'0x')||to_char(l_tag2_len,'0x')||l_tag2||
+            to_char(3,'0x')||to_char(l_tag3_len,'0x')||l_tag3||
+            to_char(4,'0x')||to_char(l_tag4_len,'0x')||l_tag4||
+            to_char(5,'0x')||to_char(l_tag5_len,'0x')||l_tag5;
+l_qr_hex := replace(l_qr_hex,' ',''); 
+l_qr_hex_raw := l_qr_hex;
+l_qr_base64 := utl_raw.cast_to_varchar2(utl_encode.base64_encode(l_qr_hex_raw));
+
+return l_qr_base64;
+end get_inv_qr_base64;
